fe36b0ce4e3dfed07c79dd3392ee2d27be7a60bc
Lanbate <37767636+Lanbate@users.noreply.github.com>
2019-06-11 11:28:16 +0800

    Update kes.js

--- config/kes.js
+++ config/kes.js
@@ -1,5 +1,5 @@
 //引用exports提供方法，输出是为一个对象，引用module.exports提供方法，输出为字符串。
 module.exports={
-    mongoURI:"mongodb+srv://Lan:qq973762649@lanbackground-cytif.azure.mongodb.net/LanBackground?retryWrites=true&w=majority",
+    mongoURI:"",
     secretOrKey:"secret"
 }
